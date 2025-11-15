62dcca06a26f5fd231ca4d4efabc7bb647136c5b
Fazzin <71731841+Fazzin@users.noreply.github.com>
2025-11-14 23:45:17 -0500

    Test Constraints with solid principles

---
+++ test_constraints.h
@@ -0,0 +1,12 @@
+#ifndef TEST_CONSTRAINTS_H
+#define TEST_CONSTRAINTS_H
+
+struct TestConstaints {
+    int numProblems;
+    int minTopicCount;
+    int maxTopicCount;
+    int minDifficulty;
+    int maxDifficulty;
+};
+
+#endif
